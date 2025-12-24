cab6144cb5c5d198213dc72519d964c005eb40e6
DrMahmoudCodes <mahmoudhanwer@gmail.com>
2025-12-24 14:07:07 +0200

    1

---
+++ story_content/triggers.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6K6EPNuQytI":
+        Script1();
+        break;
+      case "6oLK3ilII3p":
+        Script2();
+        break;
+      case "5y4PqqaaEdu":
+        Script3();
+        break;
+      case "5c3z6cPSGiX":
+        Script4();
+        break;
+      case "5cqUyWPZDDZ":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
